ffa2124c36c1310c22e708425d8858a2269749a2
minhhang411 <maominhhang114@gmail.com>
2024-09-28 02:40:37 +0700

    up

---
+++ assets/js/script.js
@@ -0,0 +1,47 @@
+
+'use strict';
+
+const elementToggleFunc = function (elem) { elem.classList.toggle("active"); }
+// contact form variables
+const form = document.querySelector("[data-form]");
+const formInputs = document.querySelectorAll("[data-form-input]");
+const formBtn = document.querySelector("[data-form-btn]");
+
+// add event to all form input field
+for (let i = 0; i < formInputs.length; i++) {
+  formInputs[i].addEventListener("input", function () {
+    // check form validation
+    if (form.checkValidity()) {
+      formBtn.removeAttribute("disabled");
+    } else {
+      formBtn.setAttribute("disabled", "");
+    }
+  });
+}
+
+// page navigation variables
+const navigationLinks = document.querySelectorAll("[data-nav-link]");
+const pages = document.querySelectorAll("[data-page]");
+
+// Hiển thị Resume mặc định khi trang được tải
+pages.forEach(page => {
+  page.classList.remove("active"); // Ẩn tất cả các trang
+});
+const resumePage = document.querySelector("[data-page='resume']");
+resumePage.classList.add("active"); // Hiển thị trang Resume
+
+// add event to all nav link
+for (let i = 0; i < navigationLinks.length; i++) {
+  navigationLinks[i].addEventListener("click", function () {
+    for (let j = 0; j < pages.length; j++) {
+      if (this.innerHTML.toLowerCase() === pages[j].dataset.page) {
+        pages[j].classList.add("active");
+        navigationLinks[j].classList.add("active");
+        window.scrollTo(0, 0);
+      } else {
+        pages[j].classList.remove("active");
+        navigationLinks[j].classList.remove("active");
+      }
+    }
+  });
+}
